fdde02eb470672838b55e978d702fdf60bfd2b3a
sol5288 <solosols@naver.com>
2024-08-05 19:42:27 +0900

    eslint, prettier 충돌해결 및 쓸데없는 로그삭제

--- chat/.eslintrc.cjs
+++ chat/.eslintrc.cjs
@@ -1,8 +1,25 @@
 module.exports = {
   root: true,
-  extends: ['@antfu'],
+  extends: ['@antfu', 'plugin:prettier/recommended'],
   rules: {
     'no-tabs': 0,
+    'prettier/prettier': [
+      'warn',
+      {
+        endOfLine: 'lf',
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'es5',
+        printWidth: 120,
+        tabWidth: 2,
+        useTabs: false,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        htmlWhitespaceSensitivity: 'css',
+        vueIndentScriptAndStyle: true,
+        vueComponentPropsOrder: ['props', 'data', 'computed', 'methods', 'watch', 'lifecycle hooks'],
+      },
+    ],
   },
   globals: {
     WeixinJSBridge: false,
